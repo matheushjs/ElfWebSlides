027131de8abfc28a35f57de6483d58b8de85106c
Matheus H. J. Saldanha <mhjsaldanha@gmail.com>
2025-08-14 04:58:01 +0900

    Finish triangles logic

--- triangles.js
+++ triangles.js
@@ -18,3 +18,23 @@ removeTitleTriangles = function() {
         node.classList.add("fade-out");
     })
 }
+
+addTocTriangles = function() {
+    var tri1 = document.querySelector(".triangle-right2");
+    var tri2 = document.querySelector(".triangle-up-right");
+    
+    [tri1, tri2].forEach(function(node){
+        node.classList.remove("fade-out");
+        node.classList.add("fade-in");
+    })
+}
+
+removeTocTriangles = function() {
+    var tri1 = document.querySelector(".triangle-right2");
+    var tri2 = document.querySelector(".triangle-up-right");
+    
+    [tri1, tri2].forEach(function(node){
+        node.classList.remove("fade-in");
+        node.classList.add("fade-out");
+    })
+}
